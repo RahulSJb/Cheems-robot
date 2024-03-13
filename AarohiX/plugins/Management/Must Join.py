4ed34853f52569a1d034fc8a41c56b87468ede37
RahulSJb <161461605+RahulSJb@users.noreply.github.com>
2024-03-13 17:17:59 +0530

    Update Must Join.py

--- AarohiX/plugins/Management/Must Join.py
+++ AarohiX/plugins/Management/Must Join.py
@@ -26,11 +26,11 @@ async def must_join_channel(app: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("</> ᴍᴜsɪᴄ ʙᴏᴛ[❣️]", url=f"https://t.me/Alone_Dil_bot?startgroup=true"),
+                                InlineKeyboardButton("</> ᴍᴜsɪᴄ ʙᴏᴛ[❣️]", url=f"https://t.me/Equinox_chat?startgroup=true"),
                             ],
                             [
                                 InlineKeyboardButton("</> sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ[❣️]", url=link),
-                                InlineKeyboardButton("</> ᴄʜᴀɴɴᴇʟ ᴄʜᴀᴛ[❣️]", url=f"https://t.me/AllPremiumBIN"),
+                                InlineKeyboardButton("</> ᴄʜᴀɴɴᴇʟ ᴄʜᴀᴛ[❣️]", url=f"https://t.me/Equinoxnetwork"),
                             ],
                         ]
                     )
